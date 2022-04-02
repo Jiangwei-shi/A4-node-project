3d603b2f9ef271a4175c5843ecd0c3dbfb7675c1
Jiangwei-shi <56906163+Jiangwei-shi@users.noreply.github.com>
2022-04-02 14:08:58 -0400

    fix bug

--- server.ts
+++ server.ts
@@ -27,8 +27,8 @@ require("dotenv").config();
 const PROTOCOL = "mongodb+srv";
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const HOST = "cluster0.pha8g.mongodb.net";
-const DB_NAME = "A4";
+const HOST = "cluster0.dgng5.mongodb.net";
+const DB_NAME = "myFirstDatabase";
 const DB_QUERY = "retryWrites=true&w=majority";
 const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;// connect to the database
 mongoose.connect(connectionString);
